22d2572f6cdd04bb0fd1b178cd50c132358f006f
emmalindauer <ejl69@drexel.edu>
2016-05-04 13:42:53 -0400

    Morse update

--- _2.2_Morse/_2.2_Morse.ino
+++ _2.2_Morse/_2.2_Morse.ino
@@ -1,9 +1,35 @@
 String ch ;
+int dotTime = 200 ;
+int dashTime = 3*dotTime ;
+int waitTime = dashTime ;
+int LED = 5 ;
+
 
 void setup() {
  Serial.begin(9600) ;
-  
+ pinMode(LED, OUTPUT) ;
+ 
 }
+
+void dot() {
+  digitalWrite(LED, HIGH) ;
+  delay(dotTime) ;
+  digitalWrite(LED, LOW) ;
+  delay(dotTime) ;
+}
+
+void dash() {
+  digitalWrite(LED, HIGH) ;
+  delay(dashTime) ;
+  digitalWrite(LED, LOW) ;
+  delay(dotTime) ;
+}
+
+void wait() {
+  delay(waitTime) ;
+}
+
+
 
 void loop() {
  if(Serial.available() > 0 ) {
@@ -15,10 +41,16 @@ void loop() {
     
     switch(ch.charAt(i)) {
       case 'a':
-      Serial.println("123") ;
+      dot() ; 
+      dash() ;
+      dot() ;
+      wait() ;
       break ;
       case 'b':
-      Serial.println("456") ;
+      dash() ;
+      dash() ;
+      dash() ;
+      wait() ;
       break ;
     }
   }
