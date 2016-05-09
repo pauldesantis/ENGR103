c1e813f15ac4bbe2edec825e6727d7cf0f90ebe9
pauldesantis <pauldesa97@gmail.com>
2016-05-09 12:01:26 -0400

    Updated 3 LEDs
    Updated to include comments

--- _1.2_3_LEDs/_1.2_3_LEDs.ino
+++ _1.2_3_LEDs/_1.2_3_LEDs.ino
@@ -1,16 +1,13 @@
-<<<<<<< HEAD
-=======
 /*
   Blink with 3 LEDs
   Turns on LED1 on for one second, then LED1 off and LED2 on for one 
-  second, then LED2 of and LED3 on for one second, then off repeatedly.
+  second, then LED2 off and LED3 on for one second, then off repeatedly.
 
   LED1 is attached to digital pin 11
   LED2 is attached to digital pin 7
   LED3 is attached to digital pin 2
   
  */
->>>>>>> origin/master
 
 // the setup function runs once when you press reset or power the board
 void setup() {
@@ -22,25 +19,15 @@ void setup() {
 
 // the loop function runs over and over again forever
 void loop() {
-<<<<<<< HEAD
-  digitalWrite(11, HIGH);   // turn the LED on (HIGH is the voltage level)
-  delay(1000);
-  digitalWrite(11, LOW);
-  //delay(1000)
-  digitalWrite(7, HIGH);   
-=======
-  digitalWrite(11, HIGH);   // turn the LED1 on by making the voltage HIGH
-  delay(1000);              // wait for a second
-  digitalWrite(11, LOW);    // turn the LED1 off by making the voltage LOW
-  digitalWrite(7, HIGH);    
->>>>>>> origin/master
-  delay(1000);
-  digitalWrite(7,LOW);
-  digitalWrite(2, HIGH);
-  delay(1000);
-  digitalWrite(2, LOW);
-<<<<<<< HEAD
-  //delay(1000)
-=======
->>>>>>> origin/master
+  digitalWrite(11, HIGH);   // turns LED1 on (HIGH is the voltage level)
+  delay(1000);              // waits 1 second (1000 microseconds)
+  
+  digitalWrite(11, LOW);    // turns LED1 off
+  digitalWrite(7, HIGH);    // turns LED2 on at the same instant LED1 turns off
+  delay(1000);              // waits 1 second
+  
+  digitalWrite(7, LOW);     // turns LED2 off
+  digitalWrite(2, HIGH);    // turns LED3 on
+  delay(1000);              // waits 1 second
+  digitalWrite(2, LOW);     // turns LED3 off
 }
