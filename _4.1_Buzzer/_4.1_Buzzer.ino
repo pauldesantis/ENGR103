1b4cf8b0bd210da0796abf62ae16fe92c5f5a561
pauldesantis <pauldesa97@gmail.com>
2016-05-11 14:28:01 -0400

    Added Buzzer File
    active & passive buzzers

---
+++ _4.1_Buzzer/_4.1_Buzzer.ino
@@ -0,0 +1,58 @@
+int x = 3;        // number of times active buzzer runs
+int active = 13;  // Active buzzer
+int passive = 9;  // Passive buzzer
+ 
+void setup() {
+  pinMode(active,OUTPUT); // active buzzer
+  pinMode(passive ,OUTPUT);
+  Serial.begin(9600);
+}
+
+void loop() {
+  switch(x){
+     // active buzzer
+    case 1:
+      // active buzzer
+      digitalWrite(active,HIGH);
+      delay(1000);
+      digitalWrite(active,LOW);
+      delay(1000);
+      break;
+      
+    // passive buzzer digital
+    case 2: 
+      unsigned char i, j;
+      while (1) {
+         for (i = 0; i <80; i++){ // Wen a frequency sound
+              digitalWrite (passive, HIGH) ;// send voice
+              delay (1) ;// Delay 1ms
+              digitalWrite (passive, LOW) ;// do not send voice
+              delay (1) ;// delay ms
+              }
+         for (j = 0; j <100; j++){ // Wen Qie out another frequency sound 
+              digitalWrite (passive, HIGH) ;// send voice
+              delay (2) ;// delay 2ms
+              digitalWrite (passive, LOW) ;// do not send voice
+              delay (2) ;// delay 2ms
+          }
+          
+      }
+      break;
+      
+      // passive buzzer tone
+     case 3:
+     int k;
+     for(k = 200; k<=800; k++){
+        tone(passive,k);
+        delay(250);
+        Serial.print("k = ");
+        Serial.println(k);
+        delay(5);
+      }
+      delay(1000);
+      
+      break;
+      }
+       
+  }
+
